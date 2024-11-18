e76c0bbfe24d38f1fa0b0c6be4ba8951a6ae07a3
sasisam007 <sasisam007@gmail.com>
2024-11-18 08:56:34 +0530

    modified for render

--- app.py
+++ app.py
@@ -36,5 +36,10 @@ def decode():
 
     return render_template('decode.html', message=None)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to port 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
